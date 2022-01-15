4398b6fa8ca1c9ca0d334107e03c45b7ebd70d90
minisounds <31378516+minisounds@users.noreply.github.com>
2022-01-14 21:59:25 -0800

    setting up screen files and folder

---
+++ domu/lib/screens/workout.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WorkoutScreen extends StatefulWidget {
+  const WorkoutScreen({Key? key}) : super(key: key);
+
+  @override
+  _WorkoutScreenState createState() => _WorkoutScreenState();
+}
+
+class _WorkoutScreenState extends State<WorkoutScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
