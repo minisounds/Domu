5c23884c048a77650be07f8d4494f7ffc62d7b3e
saarangbond <saarang@bondalapati.com>
2022-01-27 20:08:27 -0800

    conflicts

--- domu/lib/screens/workout.dart
+++ domu/lib/screens/workout.dart
@@ -15,10 +15,34 @@ class _WorkoutScreenState extends State<WorkoutScreen> {
           // Here we take the value from the MyHomePage object that was created by
           // the App.build method, and use it to set our appbar title.
           title: const Text("Workout"),
-          actions: const [
-            Text("3/7"),
-            Text("2000"),
+          actions: [
+            Container(
+              child: Text("3/7"),
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(10.0),
+            ),
+            Container(
+              child: const Text("2000"),
+              alignment: Alignment.center,
+              margin: const EdgeInsets.all(10.0),
+            ),
           ]),
+      body: SizedBox.expand(
+        child: Container(
+          child: Column(children: const [
+            Center(
+              child: Card(
+                child: SizedBox(
+                  width: 200,
+                  height: 200,
+                  child: Text("Card"),
+                )
+              ),
+            ),
+          ]),
+          alignment: Alignment.center,
+        )
+      )
     );
   }
 }
